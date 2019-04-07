92df5e5e8812550224e91ed06630bb61443746ff
alescontrela <alejandroescontrela@gmail.com>
2019-04-07 18:50:05 -0400

    refactor filter_lidar for 3D lidar scan

--- igvc_perception/src/filter_lidar/main.cpp
+++ igvc_perception/src/filter_lidar/main.cpp
@@ -3,31 +3,33 @@
 #include <ros/publisher.h>
 #include <ros/ros.h>
 #include <igvc_utils/NodeUtils.hpp>
+#include <string.h>
 
-ros::Publisher _pointcloud_pub;
+ros::Publisher filtered_pointcloud_pub;
+std::string input_topic, output_topic;
 
-double x_offset, y_offset, y_size, x_size, max_dist, back_buffer;
+double x_front, x_back;
+double y_left, y_right;
+double z_up, z_down;
+
+double maximum_distance;
 
 void point_cloud_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg)
 {
   pcl::PointCloud<pcl::PointXYZ> result;
-  double old_size = x_size;
   for (auto it = msg->points.begin(); it != msg->points.end(); it++)
   {
-    if (it->x < 0)
-    {
-      x_size += back_buffer;
-    }
-    if (!(abs(it->x + x_offset) < x_size && abs(it->y + y_offset) < y_size) &&
-        sqrt(pow(it->x, 2) + pow(it->y, 2)) < max_dist)
+    if ((((it->x >= x_front) || (it->x <= -x_back)) ||
+        ((it->y >= y_left)  || (it->y <= -y_right)) ||
+        ((it->z >= z_up)    || (it->z <= -z_down))) &&
+        (sqrt(pow(it->x, 2) + pow(it->y, 2) + pow(it->z, 2)) < maximum_distance))
     {
       result.push_back(*it);
     }
-    x_size = old_size;
   }
   result.header.frame_id = msg->header.frame_id;
   result.header.stamp = msg->header.stamp;
-  _pointcloud_pub.publish(result);
+  filtered_pointcloud_pub.publish(result);
 }
 
 int main(int argc, char** argv)
@@ -37,16 +39,22 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::NodeHandle pNh("~");
 
-  igvc::getParam(pNh, "x_offset", x_offset);
-  igvc::getParam(pNh, "y_offset", y_offset);
-  igvc::getParam(pNh, "x_size", x_size);
-  igvc::getParam(pNh, "y_size", y_size);
-  igvc::getParam(pNh, "max_dist", max_dist);
-  igvc::getParam(pNh, "back_buffer", back_buffer);
+  igvc::getParam(pNh, "input_topic", input_topic);
+  igvc::getParam(pNh, "output_topic", output_topic);
 
-  _pointcloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ> >("/scan/pointcloud", 1);
 
-  ros::Subscriber scan_sub = nh.subscribe("/pc2", 1, point_cloud_callback);
+  igvc::getParam(pNh, "x_front", x_front);
+  igvc::getParam(pNh, "x_back", x_back);
 
+  igvc::getParam(pNh, "y_left", y_left);
+  igvc::getParam(pNh, "y_right", y_right);
+
+  igvc::getParam(pNh, "z_up", z_up);
+  igvc::getParam(pNh, "z_down", z_down);
+
+  igvc::getParam(pNh, "maximum_distance", maximum_distance);
+
+  filtered_pointcloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>(output_topic, 1);
+  ros::Subscriber scan_sub = nh.subscribe(input_topic, 1, point_cloud_callback);
   ros::spin();
 }
